6525252da86f1976d7569098b5c5087716f669f2
Khushi <sparker.star001@gmail.com>
2021-08-06 00:43:51 +0530

    Added favorites features

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ let apiURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${
 console.log(startDate);
 console.log(endDate);
 
-function createDOM(resultData){
-    resultData.forEach(result => {
+function createDOM(data){
+    let renderArray = data === 'results' ? resultData : Object.values(favPosts);
+    renderArray.forEach(result => {
         // Card
         const card = document.createElement('div');
         card.classList.add('card');
-        card.innerText = result.date;
-        post.append(card);
 
         // Line
         const link = document.createElement('a');
@@ -31,8 +30,43 @@ function createDOM(resultData){
         img.loading = 'lazy';
         img.classList.add('card--image');
 
+        // Card Body
+        const cbody = document.createElement('div');
+        cbody.classList.add('card--body');
+
+        // Card Title
+        const title = document.createElement('h5');
+        title.classList.add('card--title');
+        title.innerText = result.title;
+
+        // Save Button
+        const savebtn = document.createElement('p');
+        savebtn.classList.add('clickable');
+        savebtn.style.color = 'dodgerblue';
+        if(data === 'results'){
+            savebtn.textContent = 'Add to Favorites';
+        }
+
+        // Card Text
+        const cText = document.createElement('p');
+        cText.innerText = result.explanation;
+
+        // Card Footer
+        const cFooter = document.createElement('small');
+
+        // Card Date
+        const cDate = document.createElement('strong');
+        cDate.innerText = result.date;
+
+        // Author
+        let author = result.copyright === undefined ? '' : result.copyright;
+        const cAuthor = document.createElement('span');
+        cAuthor.innerText = author;
+
+        cFooter.append(cDate, cAuthor);
+        cbody.append(title, savebtn, cText, cFooter);
         link.append(img);
-        card.append(link);
+        card.append(link, cbody);
         post.append(card);
     });
 }
@@ -40,8 +74,9 @@ function createDOM(resultData){
 async function getData(){
     try{
         const r = await fetch(apiURL);
-        const resultData = await r.json();
-        createDOM(resultData);
+        resultData = await r.json();
+        createDOM('results');
+        console.log("Done");
     } catch(error){
         console.log("Error", e);
     }
